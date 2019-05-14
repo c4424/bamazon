d5dbb3335173fe0ab2f811f2634b1754a4447386
c4424 <c4424@live.com>
2019-05-14 14:06:28 -0700

    npm packages, database added, table added

--- bamazon schema.sql
+++ bamazon schema.sql
@@ -1,15 +1,17 @@
 DROP DATABASE IF EXISTS bamazonDB;
---creates a database --
 CREATE database bamazonDB;
 USE bamazonDB;
 
---creates an invetory table for bamazonDB--
-CREATE TABLE inventory (
-  position INT NOT NULL,
-  brand VARCHAR(100) NULL,
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
   price DECIMAL (10,2) NULL,
-  stock DECIMAL (10,2) NULL,
-  PRIMARY KEY (inventory)
+  stock_quantity DECIMAL (10,2) NULL,
+  PRIMARY KEY (id)
 );
 
-SELECT * FROM inventory;
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bud Light", "NOT-SO alcoholic beverages", 5.0, 500), ("Modelo", "NOT-SO alcoholic beverages", 6.50, 500), ("Heineken", "alcoholic beverages", 6.30, 350), ("Stella Artois", "alcoholic beverages", 6.60, 450), ("Blue Moon", "alcoholic beverages", 7.50, 400), ("Guinness", "alcoholic beverages", 7.00, 300), ("Lagunitas", "alcoholic beverages", 7.50, 450), ("IceHouse", "NOT-SO alcoholic beverages", 5.30, 200), ("Lagunitas", "alcoholic beverages", 7.50, 450), ("Miller", "NOT-SO alcoholic beverages", 4.50, 200);
--- bamazon.js
+++ bamazon.js
@@ -13,18 +13,36 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
 });
- 
+
+
+
+
+function check(order, quantity){
+    connection.query(`SELECT * FROM products WHERE brand = ?`, [order], function(error, results){
+        if (error) throw error;
+        console.log(results);
+        return;
+    })
+}
+check("Bud Light", 1)
+check("Modelo", 3)
+
+
 connection.end();
 
 
 //-----------------------------------------------------//
 
-var inquirer = require('inquirer');
 
-inquirer
-  .prompt(['which type of beer would you like to buy?'
-    /* Pass your questions in here */
-  ])
-  .then(answers => { 'cheers!'
-    // Use user feedback for... whatever!!
-  });
+
+
+// var inquirer = require('inquirer');
+
+// inquirer
+//   .prompt(['which type of beer would you like to buy?'
+//     /* Pass your questions in here */
+//   ])
+//   .then(answers => { 'cheers!'
+//     // Use user feedback for... whatever!!
+//   });
+  
