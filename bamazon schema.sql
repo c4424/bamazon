0e499896df656ee93972207643b1a2d3ba3901c3
c4424 <c4424@live.com>
2019-05-18 16:59:07 -0700

    updates

--- bamazon schema.sql
+++ bamazon schema.sql
@@ -3,16 +3,16 @@ CREATE database bamazonDB;
 USE bamazonDB;
 
 CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
+  item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NULL,
   department_name VARCHAR(100) NULL,
   price DECIMAL (10,2) NULL,
   stock_quantity DECIMAL (10,2) NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (item_id)
 );
 
 
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Bud Light", "NOT-SO alcoholic beverages", 5.0, 500), ("Modelo", "NOT-SO alcoholic beverages", 6.50, 500), ("Heineken", "alcoholic beverages", 6.30, 350), ("Stella Artois", "alcoholic beverages", 6.60, 450), ("Blue Moon", "alcoholic beverages", 7.50, 400), ("Guinness", "alcoholic beverages", 7.00, 300), ("Lagunitas", "alcoholic beverages", 7.50, 450), ("IceHouse", "NOT-SO alcoholic beverages", 5.30, 200), ("Lagunitas", "alcoholic beverages", 7.50, 450), ("Miller", "NOT-SO alcoholic beverages", 4.50, 200);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (120, "Bud Light", "NOT-SO alcoholic beverages", 5.0, 500), (121, "Modelo", "NOT-SO alcoholic beverages", 6.50, 500), (122, "Heineken", "alcoholic beverages", 6.30, 350), (123, "Stella Artois", "alcoholic beverages", 6.60, 450), (124, "Blue Moon", "alcoholic beverages", 7.50, 400), (125, "Guinness", "alcoholic beverages", 7.00, 300), (126, "Lagunitas", "alcoholic beverages", 7.50, 450), (127, "IceHouse", "NOT-SO alcoholic beverages", 5.30, 200), (128, "Lagunitas", "alcoholic beverages", 7.50, 450), (129, "Miller", "NOT-SO alcoholic beverages", 4.50, 200);
 SELECT*FROM products;
--- bamazon.js
+++ bamazon.js
@@ -18,44 +18,68 @@ connection.connect(function (err){
   showProducts();
 });
 
+
 //-----------------------------------------------------------------------------------//
 //this part runs the inquirer package
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
+
+function productDetails() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "I know it's been a long day, what would you like to drink?",
+      name: "product",
+      filter: Number,
+    },
+    
+    {
+      type: "input",
+      message: "Great choice!, how many 6-packs are you taking today?",
+      name: "quantity",
+      filter: Number,
+    }
+])
+
   .then(answers => { 
     // Use user feedback for... whatever!!
   });
+}
+
+
   
 
-  function start () {
-    inquirer
-      .prompt({
-        name: 'postOrBid',
-        type: 'list',
-        message: 'Would you like to [POST] an auction or [BID] on an auction?',
-        choices: ['POST', 'BID', 'EXIT']
-      })
-      .then(function (answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.postOrBid === 'POST') {
-          postAuction()
-        } else if (answer.postOrBid === 'BID') {
-          bidAuction()
-        } else {
-          connection.end()
-        }
-      })
-  }
+  // function start () {
+  //   inquirer
+  //     .prompt({
+  //       name: 'postOrBid',
+  //       type: 'list',
+  //       message: 'Would you like to [POST] an auction or [BID] on an auction?',
+  //       choices: ['POST', 'BID', 'EXIT']
+  //     })
+  //     .then(function (answer) {
+  //       // based on their answer, either call the bid or the post functions
+  //       if (answer.postOrBid === 'POST') {
+  //         postAuction()
+  //       } else if (answer.postOrBid === 'BID') {
+  //         bidAuction()
+  //       } else {
+  //         connection.end()
+  //       }
+  //     })
+  // }
 
 
 
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
+function showProducts() {
+  connection.query('SELECT * FROM products', function (error, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log('\nItem ID ' + res[i].item_id + " | " + 'Beer brand: ' + res[i].product_name + " | " + 'Department: ' + res[i].department_name + " | " + 'Price (6-pack): ' + res[i].price.toString() + " | " + 'Stock: ' + res[i].stock_quantity.toString())
+    }    
+    console.log("----------------");
+    productDetails();
+  });
+
+} 
+
 
 
 
@@ -68,8 +92,6 @@ function check(order, quantity){
     })
 };
 
-// check("Bud Light", 1)
-// check("Modelo", 3)
 
 connection.end();
 
