f6114855e555fd3edb8f17ce380f945dcee8f4c7
c4424 <c4424@live.com>
2019-05-18 15:57:02 -0700

    some updates

--- bamazon schema.sql
+++ bamazon schema.sql
@@ -15,3 +15,4 @@ CREATE TABLE products (
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Bud Light", "NOT-SO alcoholic beverages", 5.0, 500), ("Modelo", "NOT-SO alcoholic beverages", 6.50, 500), ("Heineken", "alcoholic beverages", 6.30, 350), ("Stella Artois", "alcoholic beverages", 6.60, 450), ("Blue Moon", "alcoholic beverages", 7.50, 400), ("Guinness", "alcoholic beverages", 7.00, 300), ("Lagunitas", "alcoholic beverages", 7.50, 450), ("IceHouse", "NOT-SO alcoholic beverages", 5.30, 200), ("Lagunitas", "alcoholic beverages", 7.50, 450), ("Miller", "NOT-SO alcoholic beverages", 4.50, 200);
+SELECT*FROM products;
--- bamazon.js
+++ bamazon.js
@@ -1,4 +1,10 @@
+//the npm packages I need
 var mysql = require('mysql');
+var inquirer = require('inquirer');
+//----------------------------------------------------------------------------------//
+
+
+//this runs mysql database
 var connection = mysql.createConnection({
   host     : 'localhost',
   port     : 3306,
@@ -7,7 +13,44 @@ var connection = mysql.createConnection({
   database : 'bamazonDB'
 });
  
-connection.connect();
+connection.connect(function (err){
+  if (err) throw err;
+  showProducts();
+});
+
+//-----------------------------------------------------------------------------------//
+//this part runs the inquirer package
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => { 
+    // Use user feedback for... whatever!!
+  });
+  
+
+  function start () {
+    inquirer
+      .prompt({
+        name: 'postOrBid',
+        type: 'list',
+        message: 'Would you like to [POST] an auction or [BID] on an auction?',
+        choices: ['POST', 'BID', 'EXIT']
+      })
+      .then(function (answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.postOrBid === 'POST') {
+          postAuction()
+        } else if (answer.postOrBid === 'BID') {
+          bidAuction()
+        } else {
+          connection.end()
+        }
+      })
+  }
+
+
+
  
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
@@ -18,31 +61,26 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 
 
 function check(order, quantity){
-    connection.query(`SELECT * FROM products WHERE brand = ?`, [order], function(error, results){
+    connection.query(`SELECT * FROM products WHERE product_name = ?`, [order], function(error, results){
         if (error) throw error;
         console.log(results);
         return;
     })
-}
-check("Bud Light", 1)
-check("Modelo", 3)
+};
 
+// check("Bud Light", 1)
+// check("Modelo", 3)
 
 connection.end();
 
 
+// CHECK ON iceCreamCRUD from activity 09
+// CHECK ON greatBayBasic from activity 10
+// npm install cli-table
 //-----------------------------------------------------//
 
 
 
 
-// var inquirer = require('inquirer');
 
-// inquirer
-//   .prompt(['which type of beer would you like to buy?'
-//     /* Pass your questions in here */
-//   ])
-//   .then(answers => { 'cheers!'
-//     // Use user feedback for... whatever!!
-//   });
-  
+
