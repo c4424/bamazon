ebe27fd2862d36a27d8bfbc0edb19c026571660e
c4424 <c4424@live.com>
2019-05-18 20:00:48 -0700

    more updates

--- bamazon.js
+++ bamazon.js
@@ -39,40 +39,48 @@ function productDetails() {
     }
 ])
 
-  .then(answers => { 
-    // Use user feedback for... whatever!!
-  });
+  .then(function (res){
+    var items = res.product;
+    var productQuantity = res.quantity;
+
+    connection.query("SELECT * FROM products WHERE ?", { item_id:items }, 
+      function(err, res){
+        if (err) throw err;
+        // if they dont select a product, they will get this error message, after that the product list opens again.
+        if (response.length === 0) {
+          console.log("Please select a product ID from the list");
+          showProducts();
+        } else {
+        // this responds if the quantity selected is in stock
+          var productStock = response[0];
+          if (productQuantity <= productStock.stock_quantity) {
+            console.log("we still have this product on stock")
+
+            var updateInventory = 'UPDATE products SET stock_quantity= ' + (productStock.stock_quantity - productQuantity) + 'WHERE item_id= ' + items;
+
+            connection.query(updateInventory, function (err, data){
+              if (err) throw err;
+              console.log('Your order has been placed, your total is $' + productStock.price * productQuantity);
+              console.log('Thanks for choosing our online bar. Cheers!');
+              keepShopping();
+            })
+
+          } else {
+            console.log('We are sorry but this beer is out of stock.\n' + 'Please select a different beer.\n' + 'your beer was ' + productStock.product_name + 'and there are only ' + productStock.stock_quantity + 'in stock.');
+            keepShopping();
+          }
+        }
+      })
+  })
 }
 
 
-  
 
-  // function start () {
-  //   inquirer
-  //     .prompt({
-  //       name: 'postOrBid',
-  //       type: 'list',
-  //       message: 'Would you like to [POST] an auction or [BID] on an auction?',
-  //       choices: ['POST', 'BID', 'EXIT']
-  //     })
-  //     .then(function (answer) {
-  //       // based on their answer, either call the bid or the post functions
-  //       if (answer.postOrBid === 'POST') {
-  //         postAuction()
-  //       } else if (answer.postOrBid === 'BID') {
-  //         bidAuction()
-  //       } else {
-  //         connection.end()
-  //       }
-  //     })
-  // }
-
-
-
+// this function calls Inquirer productDetails() to show a list of the products and it uses toString() to its more readable info
 function showProducts() {
   connection.query('SELECT * FROM products', function (error, res) {
     for (var i = 0; i < res.length; i++) {
-      console.log('\nItem ID ' + res[i].item_id + " | " + 'Beer brand: ' + res[i].product_name + " | " + 'Department: ' + res[i].department_name + " | " + 'Price (6-pack): ' + res[i].price.toString() + " | " + 'Stock: ' + res[i].stock_quantity.toString())
+      console.log('\nItem ID ' + res[i].item_id + " | " + 'Beer brand: ' + res[i].product_name + " | " + 'Department: ' + res[i].department_name + " | " + 'Price (6-pack): ' + res[i].price.toString() + " | " + 'Stock: ' + res[i].stock_quantity.toString());
     }    
     console.log("----------------");
     productDetails();
@@ -80,25 +88,30 @@ function showProducts() {
 
 } 
 
-
-
-
-
-function check(order, quantity){
-    connection.query(`SELECT * FROM products WHERE product_name = ?`, [order], function(error, results){
-        if (error) throw error;
-        console.log(results);
-        return;
-    })
-};
-
-
-connection.end();
+//inquirer promps a question asking if the user wants to keep shopping and it calls the showProducts function 
+//if user chooses yes. if user chooses no then connection ends.
+function keepShopping(){
+  inquirer.prompt([
+    {
+      type: "confirm",
+      message: "do you want to keep shopping for beers?",
+      name: "confirm"
+    }
+  ]).then(function(res){
+    if (res.confirm){
+      console.log("----------------");
+      showProducts();
+    } else {
+      console.log('Thank you for shopping in our online bar, happy drinking!');
+      connection.end();
+    }
+  })
+}
 
 
 // CHECK ON iceCreamCRUD from activity 09
 // CHECK ON greatBayBasic from activity 10
-// npm install cli-table
+// try to npm install cli-table
 //-----------------------------------------------------//
 
 
